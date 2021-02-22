26e1a3723cd4dad4ba444a8d9114a265a9fcd00e
dagel_yuna <yuna0102>
2021-02-22 15:16:34 +0900

    first commit

---
+++ example01.js
@@ -0,0 +1,18 @@
+var length = 16; //number
+//private int length = 16; ##in java
+
+var lastName = "Johnson";//String
+//String lastName = "Johnson"
+
+var x = { firstname: "John", lastName: "Doe"}; //Object 구조체
+var x = 16 + "Volvo";
+
+console.log(x);
+var y= "16" + "Volve";
+console.log(y);
+var z = 16 + "Volve";
+console.log(z);
+var a = "Volvo" + 16;
+console.log(a);
+var b = 16 + 4 + "Volvo";
+console.log(b);
