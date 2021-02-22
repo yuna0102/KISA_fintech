41a451541f51b35bfa1e07d995fea48093fa5e10
dagel_yuna <yuna0102>
2021-02-22 16:06:33 +0900

    [문법] 조건문

---
+++ condition.js
@@ -0,0 +1,7 @@
+var hour = 15;
+var greeting = null;
+
+if (hour < 18) {
+    greeting = "Good day";
+}
+console.log(greeting);
---
+++ loof.js
@@ -0,0 +1,13 @@
+var cars = ["BMW", "Volve", "Saab", "Ford", "Fiat", "Audi"];
+var text ="";
+var i;
+for (i=0; i < cars.length; i++) {
+    text += cars[i];
+}
+
+//ex6
+//cars.map((car)=>{
+//     console.log(car);
+// })
+
+console.log(text);
