6bd28dfcf75c987eede09ba7261566afb356420c
dagel_yuna <yuna0102>
2021-02-22 15:27:02 +0900

    [실습] js로 사칙연산 만들기

---
+++ function.js
@@ -0,0 +1,21 @@
+//fucntion은 특정한 기능을 모아놓거나 자주 사용하는 것들을 선언해두는 것 (함수 기능)
+// function myFunction(p1, p2) { //파라미터 두 개 
+//     return p1*p2; //p1,p2 곱연산의 결과를 반환
+// }
+
+function hello(){
+    console.log("hello"); //console은 print와 비슷한 개념
+} //함수 선언하기
+
+hello(); //함수 불러오기
+hello();
+hello();
+hello();
+hello();
+
+//es6
+// const hello2 = () =>{
+//     console.log("hello2");
+// }
+
+
---
+++ practice.js
@@ -0,0 +1,21 @@
+//#wor01  사칙연산을 하는 +-/* 기능을 작성하세요
+function plus(p1, p2){
+    console.log(p1+p2);
+}
+
+function minus(p1, p2){
+    console.log(p1-p2);
+}
+
+function multiply(p1, p2){
+    console.log(p1*p2);
+}
+
+function division(p1, p2){
+    console.log(p1/p2);
+}
+
+plus(4, 2);
+minus(4, 2);
+multiply(4, 2);
+division(4, 2);
