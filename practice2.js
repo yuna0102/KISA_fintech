ceabcf20cd857c8a356a46cffe8fb5929c7095a7
dagel_yuna <yuna0102>
2021-02-22 15:39:28 +0900

    [실습] car 변수와 함수 언급

---
+++ practice2.js
@@ -0,0 +1,25 @@
+var car = {
+    name : "sonata",
+    ph : "500ph",
+    start : function() {
+        console.log("sonata is starting");
+    },
+    stop : function() {
+        console.log("sonata is stopped");
+    }
+}
+
+car.start();
+
+var car2 = {
+    name : "venz",
+    ph : "500ph",
+    start : function() {
+        console.log("venz is starting");
+    },
+    stop : function() {
+        console.log("venz is stopped");
+    }
+}
+
+car2.start();
