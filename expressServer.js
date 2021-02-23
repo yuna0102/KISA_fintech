13ec146f058e323ff5fac1f663a2d080f99142c0
dagel_yuna <yuna0102>
2021-02-23 16:47:53 +0900

    express route 추가

--- expressServer.js
+++ expressServer.js
@@ -1,5 +1,30 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+
+app.set('views', __dirname + '/views');
+//뷰파일이 있는 디렉토리 위치를 설정함
+//이 경로의 디렉토리에 있는 파일들을 불러올 것임
+app.set('view engine', 'ejs');
+//뷰엔진으로 ejs 사용한다
+
+
+// connection.end();
+app.get('/', function (req, res) {
+    res.send('Hello World');
+})
+
+app.get('/user', function (req, res) {
+    connection.query('SELECT * FROM fintech.user;', function (error, results, fields) {
+        console.log(results);
+        res.send('사용자페이지입니다.')
+    });
+})
+
+app.get('/ejs', function (req, res) {
+    res.render('ejsTest.ejs');
+})
+
+
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
     host     : 'localhost',
@@ -9,17 +34,6 @@ var connection = mysql.createConnection({
     port : '3307'
 });
 connection.connect();
-// connection.end();
 
-app.get('/', function (req, res) {
-    res.send('Hello World')
-})
-
-app.get('/user', function (req, res) {
-    connection.query('SELECT * FROM fintech.user;', function (error, results, fields) {
-        console.log(results);
-        res.send('사용자페이지입니다.')
-    });
-})
 
 app.listen(3000)
