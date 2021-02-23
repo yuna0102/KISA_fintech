c0f52a0b39bf5be6645459ed81abde7a3b914267
dagel_yuna <yuna0102>
2021-02-23 16:33:40 +0900

    mysql-js 유저정보 및 서버 게시

--- expressServer.js
+++ expressServer.js
@@ -1,20 +1,25 @@
 const express = require('express')
 const app = express()
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'phgbbynl0102^0^mysql',
+    database : 'fintech',
+    port : '3307'
+});
+connection.connect();
+// connection.end();
 
 app.get('/', function (req, res) {
     res.send('Hello World')
 })
 
 app.get('/user', function (req, res) {
-    res.send('사용자페이지입니다.')
-})
-
-app.get('/upload', function (req, res) {
-    res.send('게시물페이지입니다.')
-})
-
-app.get('/community', function (req, res) {
-    res.send('소통페이지입니다.')
+    connection.query('SELECT * FROM fintech.user;', function (error, results, fields) {
+        console.log(results);
+        res.send('사용자페이지입니다.')
+    });
 })
 
 app.listen(3000)
